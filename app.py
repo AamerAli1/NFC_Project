940a3edba77a483b95ccc09ea550ec663dd4019f
Amer Alkazemi <potatobo3@gmail.com>
2022-10-15 21:18:03 +0300

    initial design

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask , render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/amer_alkazemi')
 def home():
    return render_template('home.html')
 if __name__ == '__main__':
